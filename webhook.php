f0c2c1bf85a31ca1b36000ce6ac032f23d8dcf05
vuduchong209305 <vuduchong209305@gmail.com>
2021-03-22 15:47:56 +0700

    fix method checkout

--- webhook.php
+++ webhook.php
@@ -359,7 +359,7 @@ function avada_script_thankyou($order_id) {
 			$url = "https://app.avada.io/app/api/v1/checkouts";
 			$ch = curl_init($url);
 
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
